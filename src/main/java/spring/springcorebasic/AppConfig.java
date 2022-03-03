bad875b55b0aba4d4e04370c9277378c17237dda
jvnlee <lhjun1028@naver.com>
2022-03-03 23:08:07 +0900

    [REFACTOR] AppConfig 내의 중복 코드 제거, 역할과 구현 클래스가 잘 드러나도록 수정

--- src/main/java/spring/springcorebasic/AppConfig.java
+++ src/main/java/spring/springcorebasic/AppConfig.java
@@ -1,6 +1,8 @@
 package spring.springcorebasic;
 
+import spring.springcorebasic.discount.DiscountPolicy;
 import spring.springcorebasic.discount.FixedDiscountPolicy;
+import spring.springcorebasic.member.MemberRepository;
 import spring.springcorebasic.member.MemberService;
 import spring.springcorebasic.member.MemberServiceImpl;
 import spring.springcorebasic.member.MemoryMemberRepository;
@@ -8,13 +10,22 @@ import spring.springcorebasic.order.OrderService;
 import spring.springcorebasic.order.OrderServiceImpl;
 
 // 스프링 DI 컨테이너를 직접 구현해보기
+// 역할과 구현 클래스가 한눈에 들어오게 작성
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixedDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixedDiscountPolicy();
     }
 }
