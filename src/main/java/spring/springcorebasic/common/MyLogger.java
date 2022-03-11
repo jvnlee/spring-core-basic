c318cb7c3b35b458ee21b6d0b1410a46cb55e096
jvnlee <lhjun1028@naver.com>
2022-03-12 03:10:45 +0900

    [REFACTOR] Scope 어노테이션의 proxyMode 옵션을 사용해 프록시 객체를 의존성으로 주입 후, HTTP 요청이 들어오면 실제 Bean으로 대체

--- src/main/java/spring/springcorebasic/common/MyLogger.java
+++ src/main/java/spring/springcorebasic/common/MyLogger.java
@@ -1,6 +1,7 @@
 package spring.springcorebasic.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,11 +9,16 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
     /*
-    각 클라이언트가 보내는 개별 http 요청마다 하나씩 생성되어 각 요청이 끝나는 시점에 소멸되는 Bean
-    각 클라이언트 로그를 남기는 용도
+    @Scope 어노테이션에 proxyMode = ScopedProxyMode.TARGET_CLASS 옵션을 넣으면 이 클래스의 객체를 의존성으로 갖는 곳에
+    이 클래스를 상속받은 가짜(프록시) 객체를 만들어 주입해줌.
+
+    내부 원리:
+    스프링 컨테이너는 CGLIB이라는 바이트 코드 조작 라이브러리를 사용해서 프록시 객체를 생성함
+    Controller와 Service의 의존성 주입에는 프록시 객체가 사용되지만, 실제 HTTP 요청이 오게 되면 실제 Bean을 요청하는
+    위임 로직이 작동해서 프록시 Bean이 실제 MyLogger Bean을 찾고 자신을 실제 Bean으로 대체한 뒤에 동작이 수행됨.
     */
 
     private String uuid;
--- src/main/java/spring/springcorebasic/web/LogDemoController.java
+++ src/main/java/spring/springcorebasic/web/LogDemoController.java
@@ -1,7 +1,6 @@
 package spring.springcorebasic.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,20 +13,12 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        /*
-        MyLogger Bean은 request scope라서 클라이언트로부터 HTTP request가 왔을 때 비로소 생성이 되기 때문에 처음 스프링 컨테이너를 띄웠을 때는 존재할 수가 없다.
-        따라서 MyLogger를 직접 의존성으로 주입받으면 처음 스프링 컨테이너를 띄울 때(요청이 없는 상태), 아직 없는 Bean을 요청하는 꼴이므로 에러가 발생한다.
-
-        이런 문제를 막기 위해 ObjectProvider를 사용해서 MyLogger Bean을 나중에 받아올 수 있도록 한다.
-        ObjectProvider에게 MyLogger Bean을 구해달라는 명령을, HTTP 요청이 들어왔을 때 호출되는 메서드 logDemo()의 로직 안에 넣어 해결할 수 있다.
-        */
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
