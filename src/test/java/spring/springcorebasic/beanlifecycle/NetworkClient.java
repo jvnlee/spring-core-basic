2ce58edd41effd66a39c81bc09cca05e27ec0a86
jvnlee <lhjun1028@naver.com>
2022-03-11 21:18:20 +0900

    [SIMPLIFY] NetworkClient Bean의 초기화/종료 콜백을 위한 인터페이스 구현을 없애고 어노테이션 지정 방식으로 변경

--- src/test/java/spring/springcorebasic/beanlifecycle/NetworkClient.java
+++ src/test/java/spring/springcorebasic/beanlifecycle/NetworkClient.java
@@ -1,13 +1,9 @@
 package spring.springcorebasic.beanlifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
-/*
-초기화 인터페이스(InitializingBean)와 소멸 인터페이스(DisposableBean)을 사용해서 Bean의 life cycle 단계에 맞춰 필요한 기능을 실행할 수 있게함
-스프링 초창기의 방식이라 지금은 잘 사용하지 않음
-*/
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -34,17 +30,17 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
     // 의존 관계 주입이 끝난 후 호출
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("afterPropertiesSet() 호출");
+    @PostConstruct
+    public void init() {
+        System.out.println("init() 호출");
         connect();
         call("초기화 연결 메시지");
     }
 
     // 컨테이너가 종료될 때, 해당 Bean이 소멸하기 직전에 호출
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("destroy() 호출");
+    @PreDestroy
+    public void close() {
+        System.out.println("close() 호출");
         disconnect();
     }
 }
